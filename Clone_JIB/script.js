ddc95bfff24e5aa931664b4ade4695e459e28054
KoonPorZaLearn <134169209+KoonPorZaLearn@users.noreply.github.com>
2023-10-19 21:43:31 +0700

    Update
    Create card for data

--- Clone_JIB/script.js
+++ Clone_JIB/script.js
@@ -1,23 +1,24 @@
-fetch("http://localhost:5000/products")
-  .then((response) => {
-    if (response.ok) {
-      // Parse the response data as JSON
-      return response.json();
-    } else {
-      throw new Error("API request failed");
-    }
-  })
-  .then((data) => {
-    // Process the response data
-    data.map((item) => {
-      console.info(item.image);
-    });
-    renderProducts(data);
-  })
-  .catch((error) => {
-    // Handle any errors
-    console.error(error);
-  });
+// fetch("http://localhost:5000/products")
+//   .then((response) => {
+//     if (response.ok) {
+//       // Parse the response data as JSON
+//       return response.json();
+//     } else {
+//       throw new Error("API request failed");
+//     }
+//   })
+//   .then((data) => {
+//     // Process the response data
+//     data.map((item) => {
+//       console.info(item.image);
+//     });
+//     // RENDER PRODUCTS
+//     // renderProducts(data);
+//   })
+//   .catch((error) => {
+//     // Handle any errors
+//     console.error(error);
+//   });
 
 function renderProducts(data) {
   const productsContainer = document.getElementById("products-container");
@@ -35,3 +36,22 @@ function renderProducts(data) {
   });
   productsContainer.innerHTML += productItemsHTML;
 }
+
+// Get API Data for Test
+document.addEventListener("DOMContentLoaded", () => {
+  const getDataButton = document.getElementById("getData");
+  const resultDiv = document.getElementById("result");
+
+  getDataButton.addEventListener("click", () => {
+    // เรียกใช้งาน API จาก Express.js บนพอร์ต 5000
+    fetch("http://localhost:5000/upload")
+      .then((response) => response.json())
+      .then((data) => {
+        resultDiv.innerHTML = JSON.stringify(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        resultDiv.innerHTML = "เกิดข้อผิดพลาดในการดึงข้อมูลจาก API";
+      });
+  });
+});
